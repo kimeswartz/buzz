4b7eed819711e55906fa5c077eaac461c3583995
Kim E <107942229+kimeswartz@users.noreply.github.com>
2023-05-04 08:30:13 +0200

    Create buzzy.js

---
+++ buzzy.js
@@ -0,0 +1,27 @@
+class FizzBuzz {
+
+  public static void main(String[] args) {
+
+    for (int i = 1; i <= 100; i++) {
+      //for loop that starts at 1 and itarates to 100
+      if (i%3==0 && i%5==0){ 
+        System.out.println("FizzBuzz");
+      //prints if the number is multiple of 3 and 5
+      } else if {
+        (i%3==0)
+        System.out.println("Fizz");
+      //prints if the number is multiple of 3
+      } else if {
+        (i%5==0)      
+         System.out.println("Buzz");
+      //prints if the number is multiple of 3
+      } else {
+        System.out.println(i);
+      }
+
+
+    }
+
+  }
+
+}
